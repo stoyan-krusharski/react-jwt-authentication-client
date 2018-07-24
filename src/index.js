26f037d00e820e703c991d8e8a8b37c005d9fad2
stoyan-krusharski <stoyan@krusharski.com>
2018-07-24 10:35:11 +0200

    Project complete

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
 const store = createStore(
   reducers,
@@ -26,6 +28,8 @@ ReactDOM.render(
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={Signup} />
         <Route path="/feature" component={Feature} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
